8ed666660f1a161bf2bebf7c5634f7a8b20a28d9
gycie <gkassoumbaisidk@groupeisi.com>
2023-01-13 21:55:35 +0000

    premier

---
+++ src/test/java/com/example/gitAction/GitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
